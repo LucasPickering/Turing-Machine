8766eaaa86280d999b58a96077f78052956e9d1e
Lucas Pickering <lucas@lucaspickering.me>
2019-07-01 22:00:24 -0400

    Iterator cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 mod stack;
 mod turing;
 
-use std::collections::HashMap;
 use turing::TuringMachine;
 
 fn main() {
-    let tm = TuringMachine::new(HashMap::new());
+    let tm = TuringMachine::new(&[]);
     tm.run("".to_string());
 }
--- src/turing.rs
+++ src/turing.rs
@@ -1,6 +1,7 @@
 use crate::stack::SmInstruction::{self, *};
 use crate::stack::StackMachine;
 use std::io;
+use std::iter;
 
 /// The general strategy use here,  and almost all of the Rocketlang code, was
 /// created by Dr. Kevin Gold. The strategy is to simulate a two-stack PDA by
@@ -12,25 +13,25 @@ use std::io;
 /// room for computations.
 
 // Will be truncated to 7 bits (ASCII) to maximize stack length when encoded to an int
-type Char = u8;
+pub type Char = u8;
 
 /// This is not the most common way of defining a TM (usually you write AND
 /// move in each transition), but this is how KG taught us, and who am I to
 /// question him.
-enum TapeInstruction {
+pub enum TapeInstruction {
     Left,
     Right,
     Write(Char),
 }
 
-struct State<'a> {
+pub struct State<'a> {
     /// Unique numerical ID for this state
     id: u32,
     /// All transitions that can be made from this state
     transitions: Vec<Transition<'a>>,
 }
 
-struct Transition<'a> {
+pub struct Transition<'a> {
     /// The character on the tape that triggers this transition
     input_char: Char,
     /// The instruction to execute on the tape (L/R/W)
@@ -44,7 +45,6 @@ struct Transition<'a> {
 ///
 /// This has the external API of a TM, but internally only uses the two-variable
 /// stack machine from Rocketlang.
-///
 
 pub struct TuringMachine {
     instructions: Vec<SmInstruction>,
@@ -89,6 +89,8 @@ impl TuringMachine {
         ]
     }
 
+    /// Generates a While instruction to be the main loop of the program.
+    /// Includes logic for each state and transition.
     fn compile_main_loop(states: &[State]) -> SmInstruction {
         // TM state at the start of each iteration:
         // var_active: State ID #
@@ -96,28 +98,27 @@ impl TuringMachine {
         // Stack:
         // --
 
-        let instrs = Vec::new();
-
-        // Prelude
-        instrs.append(&mut vec![
-            // Store var_active (left tape) on the stack, then reset it to 0
-            Swap,
-            PushActive,
-            PushZero,
-            PopToActive,
-            Swap,
-        ]);
-
-        // Generate code for each state and add it to the loop. Exactly one
-        // state will be executed on each iteration, or if none match, then
-        // we'll terminate.
-        for state in states {
-            instrs.append(&mut Self::compile_state(state))
-        }
-
-        While(instrs)
+        While(
+            // Prelude
+            vec![
+                // Store var_active (left tape) on the stack, then reset it to 0
+                Swap,
+                PushActive,
+                PushZero,
+                PopToActive,
+                Swap,
+            ]
+            .into_iter()
+            // Generate code for each state and add it to the loop. Exactly one
+            // state will be executed on each iteration, or if none match, then
+            // we'll terminate.
+            .chain(states.iter().map(Self::compile_state).flatten())
+            .collect(),
+        )
     }
 
+    /// Compiles logic for a single state, including the Decr needed to check
+    /// before the If, and the If itself with all internal logic.
     fn compile_state(state: &State) -> Vec<SmInstruction> {
         // var_active holds the state counter. It starts at n (current state
         // #), and counts down to 0. Once it hits 0, it will be at the nth
@@ -129,47 +130,117 @@ impl TuringMachine {
         // -- Character under tape head (head char)
         // -- ...Right tape
 
-        let state_body = Vec::new();
-
         // Setup logic for switching on the head char
-        state_body.append(&mut vec![
-            PopToActive, // Pop the left tape to var_active
-            Swap, // Move left tape to var_inactive, var_active is now empty
-            PopToActive, // var_active has the head char
-            // Stack is now JUST the right tape
-            Swap,       // var_active has the left tape again
-            PushActive, // Left tape is back on stack (still encoded)
-        ]);
+        vec![
+            DecrActive, // Step down to the next state ID that we're checking
+            If(vec![
+                PopToActive, // Pop the left tape to var_active
+                Swap, // Move left tape to var_inactive, var_active is now empty
+                PopToActive, // var_active has the head char
+                // Stack is now JUST the right tape
+                Swap,       // var_active has the left tape again
+                PushActive, // Left tape is back on stack (still encoded)
+            ]
+            .into_iter()
+            // var_active: Trash
+            // var_inactive: Head char
+            // Stack:
+            // -- Left tape (encoded)
+            // -- ...Right tape
+            // Now we're going to check for a transition on each character.
+            // For each char that we have a transition for, we reset var_active to
+            // 0, then incr up to the char value and use an If to check against
+            // the head char.
+            // TODO: Optimize this by sorting the chars and not resetting each time
+            // NOTE: The logic here for iterating over the characters is slightly
+            // different from KG's version (I thought this was simpler).
+            .chain(
+                state
+                    .transitions
+                    .iter()
+                    .map(Self::compile_transition_check)
+                    .flatten(),
+            )
+            .collect()),
+        ]
+    }
 
-        // TM state now:
-        // var_active: Garbage
+    /// Generates code to check the next character transition, and execute that
+    /// transition if it matches.
+    fn compile_transition_check(transition: &Transition) -> Vec<SmInstruction> {
+        vec![
+            // Reset var_active to 0
+            PushZero,
+            PopToActive,
+        ]
+        .into_iter()
+        // Set var_active = transition_char
+        .chain(iter::repeat(IncrActive).take(transition.input_char as usize))
+        // var_active: Transition char (char to match head against)
+        // var_inactive: Head char
+        // Stack:
+        // -- Left tape (encoded)
+        // -- ...Right tape
+        // Add the write/move/next state code for this transition.
+        // This will execute only if the transition char matches the head.
+        // Once the If matches, we know var_active = var_inactive, so we
+        // can trash one.
+        .chain(iter::once(If(Self::compile_transition(transition))))
+        .collect()
+    }
+
+    /// Compiles a singular state transition into a series of instructions.
+    /// This is non-conditional: if these instructions are executing, we know
+    /// the transition is valid. This will apply the relevant tape instruction,
+    /// then set the next state.
+    fn compile_transition(transition: &Transition) -> Vec<SmInstruction> {
+        // var_active: Trash
         // var_inactive: Head char
         // Stack:
         // -- Left tape (encoded)
         // -- ...Right tape
 
-        // Now we're going to check for a transition on each character.
-        // For each char that we have a transition for, we reset var_active to
-        // 0, then incr up to the char value and use an If to check against
-        // the head char.
-        // TODO: Optimize this by sorting the chars and not resetting each time
+        vec![
+            PopToActive, // Pop left tape
+            Swap,
+            PushActive, // Push head char
+        ]
+        .into_iter()
+        // var_active: Trash
+        // var_inactive: Left tape (encoded)
+        // Stack:
+        // -- Head char
+        // -- ...Right tape
+        .chain(Self::compile_tape_instruction(&transition.tape_instruction))
+        // TODO: Go to next state
+        .collect()
+    }
 
-        for transition in state.transitions {
-            state_body.append(&mut vec![
-                // Reset var_active to 0
+    /// Compiles a single tape instruction (L/R/W) into a series of SM
+    /// instructions. This returns a dynamic type so that the different match
+    /// arms can return different iterators.
+    fn compile_tape_instruction(
+        tape_instruction: &TapeInstruction,
+    ) -> Vec<SmInstruction> {
+        match tape_instruction {
+            TapeInstruction::Left => vec![],  // TODO
+            TapeInstruction::Right => vec![], // TODO
+
+            TapeInstruction::Write(c) => vec![
+                // Remove the head char and replace it with a new one
+                PopToActive,
                 PushZero,
                 PopToActive,
-            ]);
-
-            // Set var_active = c
-            for _ in 0..transition.input_char {
-                state_body.push(IncrActive);
-            }
-
-            // Add the write/move/next state code for this transition
-            state_body.push(If(vec![]));
+            ]
+            .into_iter()
+            .chain(iter::repeat(IncrActive).take(*c as usize))
+            .chain(vec![PushActive])
+            .collect(),
+            // var_active: Trash
+            // var_inactive: Left tape (encoded)
+            // Stack:
+            // -- NEW head char
+            // -- ...Right tape
         }
-
-        vec![DecrActive, If(state_body)]
     }
 }
